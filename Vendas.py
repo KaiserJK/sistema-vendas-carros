4f24dcbb2769bf62c18da0c38039a892a1fdd5b6
KaiserJK <kawannkaiser@gmail.com>
2026-05-19 17:04:25 -0300

    Inicializa sistema de vendas de carros

---
+++ Vendas.py
@@ -0,0 +1,103 @@
+import json
+
+
+def carregar_dados(arquivo):
+    with open(arquivo, 'r') as f:
+        return json.load(f)
+
+
+def salvar_dados(arquivo, dados):
+    with open(arquivo, 'w') as f:
+        json.dump(dados, f, indent=4)
+
+
+def CadastrarCarro(Modelo, Marca):
+
+    carros = carregar_dados('Carros.json')
+
+    carros[Modelo] = Marca
+
+    salvar_dados('Carros.json', carros)
+
+    print("Carro cadastrado com sucesso!")
+
+
+def CadastrarCliente(CPF, Nome):
+
+    clientes = carregar_dados('Clientes.json')
+
+    clientes[CPF] = Nome
+
+    salvar_dados('Clientes.json', clientes)
+
+    print("Cliente cadastrado com sucesso!")
+
+
+def CadastrarFuncionario(Idfuncionario, Nome):
+
+    funcionarios = carregar_dados('funcionarios.json')
+
+    funcionarios[Idfuncionario] = Nome
+
+    salvar_dados('funcionarios.json', funcionarios)
+
+    print("Funcionário cadastrado com sucesso!")
+
+
+def Vendas(Vendedor, Cliente, Carro):
+
+    vendas = carregar_dados('Vendas.json')
+
+    id_venda = str(len(vendas) + 1)
+
+    vendas[id_venda] = {
+        "Vendedor": Vendedor,
+        "Cliente": Cliente,
+        "Carro": Carro
+    }
+
+    salvar_dados('Vendas.json', vendas)
+
+    print("Venda registrada com sucesso!")
+
+
+Func = int(input(
+    'Informe o que deseja usar\n'
+    'Para cadastrar um carro digite "1"\n'
+    'Para cadastrar um funcionário digite "2"\n'
+    'Para cadastrar um cliente digite "3"\n'
+    'Para realizar uma venda digite "4"\n'
+    'Entrada do usuario: '
+))
+
+if Func == 1:
+
+    CadastrarCarro(
+        input("Qual o modelo do veículo? "),
+        input("Qual a marca do carro? ")
+    )
+
+elif Func == 2:
+
+    CadastrarFuncionario(
+        input("Qual ID do funcionário? "),
+        input("Qual o nome do funcionário? ")
+    )
+
+elif Func == 3:
+
+    CadastrarCliente(
+        input("Qual o CPF do cliente? "),
+        input("Qual o nome do cliente? ")
+    )
+
+elif Func == 4:
+
+    Vendas(
+        input("Qual o vendedor? "),
+        input("Qual o cliente? "),
+        input("Qual o carro vendido? ")
+    )
+
+else:
+    print("Opção inválida!")
